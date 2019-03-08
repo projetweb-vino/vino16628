2af7b65a4ccec8dab24acdc02bd3da9dc75d73f6
OKBA BENAISSA <okbabenaisa@hotmail.com>
2019-03-07 20:35:54 -0500

    modifier la requete dans la fonction sauvgarderModife

--- modeles/Bouteille.class.php
+++ modeles/Bouteille.class.php
@@ -196,7 +196,7 @@ class Bouteille extends Modele {
 	{
 		
 			
-		$requete = "UPDATE vino__cellier SET quantite = GREATEST(quantite + ". $quantite. ", 0) ,  date_achat = ".$dateachat." ,  notes = ".$notes.",  Garde = ".$Garde.",  prix = ".$prix.",  millesime = ".$mille." WHERE id = ". $id;
+		$requete = "UPDATE vino__cellier SET quantite = $quantite ,  date_achat = '$dateachat' ,  notes = '$notes',  garde_jusqua = $Garde ,  prix = $prix,  millesime = $mille WHERE id = $id";
 		//echo $requete;
         $res = $this->_db->query($requete);
        	
