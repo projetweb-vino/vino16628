ff5245f8fd1d10ba6657ff1f364006ff548092fe
Galina Prima <48103854+GalinaPrima@users.noreply.github.com>
2019-03-14 11:56:09 -0400

    Changer l'aspect de l'affichage du login

--- vues/login.php
+++ vues/login.php
@@ -34,21 +34,25 @@
     <link href="signin.css" rel="stylesheet">
   </head>
   <body class="text-center">
-    <form class="form-signin">
-  <img class="mb-4" src="/docs/4.3/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
-  <h1 class="h3 mb-3 font-weight-normal">Connexion</h1>
-  <label for="inputEmail" class="sr-only">Nom usager</label>
-  <input type="text" id="inputEmail" class="form-control" placeholder="Nom usager" required autofocus>
-  <label for="inputPassword" class="sr-only">Mot de passe</label>
-  <input type="password" id="inputPassword" class="form-control" placeholder="Mot de passe" required>
-  <div class="checkbox mb-3">
-    <label>
-      <input type="checkbox" value="remember-me">Se souvenir de moi
-    </label>
-  </div>
-  <button class="btn btn-lg btn-primary btn-block" type="submit">Se connecter</button>
-  <p class="mt-5 mb-3 text-muted">&copy; 2019-2020</p>
-</form>
+  	<div class="container">
+  		<div class="col-lg-4 mx-auto">
+		    <form class="form-signin">
+			  <img class="mb-4" src="/docs/4.3/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
+			  <h1 class="h3 mb-3 font-weight-normal">Connexion</h1>
+			  <label for="inputEmail" class="sr-only">Nom usager</label>
+			  <input type="text" id="inputEmail" class="form-control" placeholder="Nom usager" required autofocus>
+			  <label for="inputPassword" class="sr-only">Mot de passe</label>
+			  <input type="password" id="inputPassword" class="form-control" placeholder="Mot de passe" required>
+			  <div class="checkbox mb-3">
+			    <label>
+			      <input type="checkbox" value="remember-me">Se souvenir de moi
+			    </label>
+			  </div>
+			  <button class="btn btn-lg btn-outline-danger" type="submit">Se connecter</button>
+			  <p class="mt-5 mb-3 text-muted">&copy; 2019-2020</p>
+			</form>
+		</div>
+	</div>
 </body>
 </html>
 
