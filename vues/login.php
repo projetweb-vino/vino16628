9b446f4cf3f11144ebef23153020fc68fd44e59d
Galina Prima <48103854+GalinaPrima@users.noreply.github.com>
2019-03-14 11:48:20 -0400

    Modifier les labels en Français

--- vues/login.php
+++ vues/login.php
@@ -36,18 +36,18 @@
   <body class="text-center">
     <form class="form-signin">
   <img class="mb-4" src="/docs/4.3/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
-  <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
-  <label for="inputEmail" class="sr-only">Email address</label>
-  <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-  <label for="inputPassword" class="sr-only">Password</label>
-  <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+  <h1 class="h3 mb-3 font-weight-normal">Connexion</h1>
+  <label for="inputEmail" class="sr-only">Nom usager</label>
+  <input type="text" id="inputEmail" class="form-control" placeholder="Nom usager" required autofocus>
+  <label for="inputPassword" class="sr-only">Mot de passe</label>
+  <input type="password" id="inputPassword" class="form-control" placeholder="Mot de passe" required>
   <div class="checkbox mb-3">
     <label>
-      <input type="checkbox" value="remember-me"> Remember me
+      <input type="checkbox" value="remember-me">Se souvenir de moi
     </label>
   </div>
-  <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-  <p class="mt-5 mb-3 text-muted">&copy; 2017-2019</p>
+  <button class="btn btn-lg btn-primary btn-block" type="submit">Se connecter</button>
+  <p class="mt-5 mb-3 text-muted">&copy; 2019-2020</p>
 </form>
 </body>
 </html>
