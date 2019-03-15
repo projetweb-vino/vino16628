ea1d95f882cc38822fcfeebc5fbdba018a497c84
Galina Prima <48103854+GalinaPrima@users.noreply.github.com>
2019-03-15 11:10:24 -0400

    ajouter fonction cellierUsager modifier les case et ajouter case cellier

--- Controler.class.php
+++ Controler.class.php
@@ -49,98 +49,126 @@ class Controler
 					case 'boireBouteilleCellier':
 						$this->boireBouteilleCellier();
 						break;
-					case "Login":
-					// var_dump('login');                            
-		        	if(isset($_REQUEST["username"]) && isset($_REQUEST["password"]))
-		        	{
-		        		$usager = new Usager();
-           				$data = $usager->Authentification($_REQUEST["username"],$_REQUEST["password"]);
-		        		
-		        		if($data)
-		        		{
-		        			$_SESSION["UserID"] = $data["id"];
-                            $_SESSION["UserName"] = $_REQUEST["username"];
-//                            $_SESSION["Role"] = $user['idRole'];
-		        			header("Location: ".URL_ROOT."?requete=cellier");
-		        		}
-		        		else
-		        		{
-		        			$messageErreur = "Mauvaise combinaison username/password";
-		        			require_once(__DIR__."/vues/login.php");
-		        		}
-		        	}
-		        	else
-		        	{
-		        		require_once(__DIR__."/vues/login.php");
-		        	}
-		        		break; 
-		        	case "Enregistrer":
-                    $err = array();
-		        	if(isset($_REQUEST["username"]) && isset($_REQUEST["password"]) && isset($_REQUEST["nom"]) && isset($_REQUEST["prenom"]) && !isset($_SESSION["UserID"]))
-		        	{
-		        		if(!preg_match("/^[a-zA-Z0-9]+$/",$_REQUEST['username']))
-                        {
-                            $err[] = "Login peut etre seulement de lettre et chifres";
-                        }
-                        if(strlen($_REQUEST['username']) < 3 or strlen($_REQUEST['username']) > 30)
-                        {
-                            $err[] = "Login dois avoir plus au moin du 3 simboles et pas plus 30";
-                        }
 
-                          # on verifi, si existe usager avec la meme nom
-                        global $connexion;
-                        $requete = "SELECT COUNT(id) as c FROM vino_usagers WHERE username='".$_REQUEST['username']."'";
-                        $resultat = mysqli_query($connexion, $requete);
-                        $resultat = $resultat->fetch_assoc();  
-                        if($resultat['c'] > 0)
-                        {
-                            $err[] = "Usager avec la meme nom existe";
-                        }
-                        if(count($err) == 0)
-                        {
-                            if(Enregistrer($_REQUEST['username'], $_REQUEST['password'], $_REQUEST['nom'], $_REQUEST['prenom'])) 
-                            { 
-                                $_SESSION["UserID"] = $_REQUEST["username"];
-                                header("Location: ".URL_ROOT."?requete=cellier");
-                            }
-                        }
-                        else{
-                            $messageErreur = implode(',', $err);
-                            require_once(__DIR__."/vues/formEnregistrer.php");
-                            }
-	                    }
-	                    else
-	                    {
-	                        require_once(__DIR__."/vues/formEnregistrer.php");
-	                    }
-			         	break;
+					case "Login":
+						// var_dump('login');                            
+			        	if(isset($_REQUEST["username"]) && isset($_REQUEST["password"]))
+			        	{
+			        		$usager = new Usager();
+	           				$data = $usager->Authentification($_REQUEST["username"],$_REQUEST["password"]);
+			        		
+			        		if($data)
+			        		{
+			        			$_SESSION["UserID"] = $data["id"];
+	                            $_SESSION["UserName"] = $_REQUEST["username"];
+	//                            $_SESSION["Role"] = $user['idRole'];
+			        			$this->cellierUsager($_SESSION["UserID"]);
+			        		}
+			        		else
+			        		{
+			        			$messageErreur = "Mauvaise combinaison username/password";
+			        			require_once(__DIR__."/vues/login.php");
+			        		}
+			        	}
+			        	else
+			        	{
+			        		require_once(__DIR__."/vues/login.php");
+			        	}
+		        		break; 
+
+
+		        	case "Enregistrer":
+			        	// var_dump(($_REQUEST["username"]), ($_REQUEST["password"]),($_REQUEST["nom"]), ($_REQUEST["prenom"]) ,($_SESSION["UserID"]));
+	                    $err = array();
+
+			        	if(isset($_REQUEST["username"]) && isset($_REQUEST["password"]) && isset($_REQUEST["nom"]) && isset($_REQUEST["prenom"]) && !isset($_SESSION["UserID"]))
+			        	{
+			        		if(!preg_match("/^[a-zA-Z0-9]+$/",$_REQUEST['username']))
+	                        {
+	                            $err[] = "Login peut etre seulement de lettre et chifres";
+	                        }
+	                        if(strlen($_REQUEST['username']) < 3 or strlen($_REQUEST['username']) > 30)
+	                        {
+	                            $err[] = "Login dois avoir plus au moin du 3 simboles et pas plus 30";
+	                        }
+
+	                          # on verifi, si existe usager avec la meme nom
+	                        
+	                        // $requete = "SELECT COUNT(id) as c FROM vino_usagers WHERE username='".$_REQUEST['username']."'";
+	                       
+	                        // $resultat = $resultat->fetch_assoc(); 
+	                        $usager = new Usager();
+							// Faire appel à la fonction de sauvegarde
+							$resultat = $usager->testUser($_REQUEST['username']);
+							
+							var_dump($resultat['c']);
+	                        if($resultat['c'] > 0)
+	                        {
+	                            $err[] = "Usager avec la meme nom existe";
+	                        }
+	                        if(count($err) == 0)
+	                        {
+	                        	$Enregistrer = $usager->Enregistrer($_REQUEST['username'], $_REQUEST['password'], $_REQUEST['nom'], $_REQUEST['prenom']);
+	                            if($Enregistrer) 
+	                            { 
+	                                $_SESSION["UserID"] = $_REQUEST["username"];
+	                                $this->cellierUsager($_SESSION["UserID"]);
+	                            }
+	                        }
+	                        else{
+	                            $messageErreur = implode(',', $err);
+	                            require_once(__DIR__."/vues/formEnregistrer.php");
+	                        }
+		                }
+		                else
+		                {
+		                        require_once(__DIR__."/vues/formEnregistrer.php");
+		                }
+				    	break;
+
+
+				    	case 'cellier':
+						    $this->cellierUsager($_SESSION["UserID"]);
+		                    require_once(__DIR__."/vues/cellier.php");
+						break; 
 			         	case "Logout":                           
-//delete la session en lui assignant un tableau vide
-		        	$_SESSION = array();
-//delete le cookie de session en créant un nouveau cookie avec la date d'expiration dans le passé
-		        	if(isset($_COOKIE[session_name()]))
-		        	{
-		        		setcookie(session_name(), '', time() - 3600);
-		        	}
-//détruire la session
-		        	session_destroy();
-		        	require_once(__DIR__."/vues/login.php");
-                break;     		
-					default:
-						$this->accueil();
-						break;
-				}
-			}
-			// Sinon on affiche l'accueil
-			else{
-				$this->accueil();
-			}		
-		}
+						//delete la session en lui assignant un tableau vide
+						        	$_SESSION = array();
+						//delete le cookie de session en créant un nouveau cookie avec la date d'expiration dans le passé
+						        	if(isset($_COOKIE[session_name()]))
+						        	{
+						        		setcookie(session_name(), '', time() - 3600);
+						        	}
+						//détruire la session
+						        	session_destroy();
+						        require_once(__DIR__."/vues/login.php");
+				                break;     		
+									default:
+										$this->accueil();
+									break;
+								}
+							}
+							// Sinon on affiche l'accueil
+							else{
+								$this->accueil();
+							}		
+						}
+
 
 		private function accueil()
 		{
 			$bte = new Bouteille();
             $data = $bte->getListeBouteilleCellier();
+			include("vues/entete.php");
+			include("vues/cellier.php");
+			include("vues/pied.php");
+                  
+		}
+
+		private function cellierUsager($idUsager)
+		{
+			$bte = new Usager();
+            $data = $bte->getCellier($idUsager);
 			include("vues/entete.php");
 			include("vues/cellier.php");
 			include("vues/pied.php");
