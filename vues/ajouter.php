84eafeca92752c154ea55183321b9d1216040e66
foudil-benzaid <48103976+foudil-benzaid@users.noreply.github.com>
2019-03-18 14:44:22 -0400

    Modifier le formulaire d'ajout d'une bouteille

--- vues/ajouter.php
+++ vues/ajouter.php
@@ -1,6 +1,6 @@
 <div class="ajouter">
 
-    <div class="nouvelleBouteille" vertical layout>
+    <!-- <div class="nouvelleBouteille" vertical layout>
         Recherche : <input type="text" name="nom_bouteille">
         <ul class="listeAutoComplete">
 
@@ -16,5 +16,82 @@
             </div>
             <button name="ajouterBouteilleCellier">Ajouter la bouteille</button>
         </div>
+    </div> -->
+    <div class="container contact-form col-md-6">
+
+        <form method="GET">
+
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="cellier">Celliers :  </label>
+                    <select class="form-control" name="cellier">
+                    <?php foreach ($data as $key => $cellier) { ?>
+                        <option value="<?php echo $cellier['id'] ?>"><?php echo $cellier['nom'] ?></option>
+                        <?php } ?>
+                    </select>
+                </div>
+            </div>
+
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="cellier">Type :  </label>
+                    <select class="form-control" name="cellier">
+                    <?php foreach ($data as $key => $type) { ?>
+                        <option value="<?php echo $type['id'] ?>"><?php echo $type['nom'] ?></option>
+                        <?php } ?>
+                    </select>
+                </div>
+            </div>
+
+            <div class="form-row">
+                <div class="form-group col-md-12">
+                    <label for="nom">Nom</label>
+                    <input type="text" class="form-control" id="nom" placeholder="Nom" name="nom">
+                </div>
+            </div>
+
+            <div class="form-row">
+                <div class="form-group col-md-4">
+                    <label for="millesime">Millesime</label>
+                    <input type="text" class="form-control" id="millesime" placeholder="Millesime" name="millesime">
+                </div>
+
+                <div class="form-group col-md-4">
+                    <label for="quantite">Quantité</label>
+                    <input type="number" class="form-control" id="quantite" placeholder="Quantité" name="quantite">
+                </div>
+
+                <div class="form-group col-md-4">
+                    <label for="date_achat">Date d'achat</label>
+                    <input type="date" class="form-control" id="date_achat" placeholder="Date d'achat" name="date_achat">
+                </div>
+          
+            </div>
+             <div class="form-row">
+                    <div class="form-group col-md-2">
+                        <label for="prix">Prix</label>
+                        <input type="text" class="form-control" id="prix" name="prix" name="prix_saq">
+                    </div>
+
+                    <div class="form-group col-md-4">
+                        <label for="garde_jusqua">Garder jusqu'à</label>
+                        <input type="date" class="form-control" id="garde_jusqua" name="garde_jusqua">
+                    </div>
+            
+                    <div class="form-group col-md-6">
+                        <label for="notes">Notes</label>
+                        <input type="text" class="form-control" id="notes" name="notes">
+                    </div>
+            </div>
+            <div class="form-row col-md-4">
+                <div class="form-group">
+                    <input type="hidden" name="requete" value="ajouterBouteilleNonListe">
+                    <button type="submit"  class="btn btn-primary">Ajouter</button>
+                </div>
+            </div>
+     
+        </form>
+
     </div>
+    
 </div>
