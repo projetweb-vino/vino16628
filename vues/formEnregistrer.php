0a36224a6f247bb57d374ce3be79efec75fc15e1
foudil-benzaid <48103976+foudil-benzaid@users.noreply.github.com>
2019-03-23 20:44:15 -0400

    MAJ de la page formulaire enregistrer

--- vues/formEnregistrer.php
+++ vues/formEnregistrer.php
@@ -11,72 +11,99 @@
     <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-    <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        -ms-user-select: none;
-        user-select: none;
-      }
-
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
-    </style>
     <!-- Custom styles for this template -->
     <link href="signin.css" rel="stylesheet">
   </head>
   <body class="text-center">
-  	<div class="container">
-  		<div class="col-lg-4 mx-auto">
-  			<?php
-				if(!isset($_SESSION["UserID"]))
-				{
-			?>
-		
-		    <form method="POST" class="form-signin" action="<?php echo URL_ROOT; ?>index.php">
-			  <img class="mb-4" src="/docs/4.3/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
-			  <h1 class="h3 mb-3 font-weight-normal">Enregistrer </h1>
-			  <label for="inputEmail" class="sr-only">Nom usager</label>
-			  <input type="text" id="inputName" name="username" class="form-control" placeholder="Nom usager" required autofocus>
+    <div class="container">
+      <div class="col-lg-6 mx-auto">
+        <?php
+        if(!isset($_SESSION["UserID"]))
+        {
+      ?>
+    
+       
+    <form role="form" method="POST" class="form-signin" action="<?php echo URL_ROOT; ?>index.php" >
+      <h2>S'enregistrer</h2>
+      <hr class="colorgraph">
+      <div class="row">
+        <div class="col-xs-12 col-sm-6 col-md-6">
+          <div class="form-group">
+            <input type="text" name="nom" id="nom" class="form-control input-lg" placeholder="Nom" tabindex="1" required>
+            <!-- Afficher un message d'erreur s'il y'a lieu -->
+            <?php  if(isset($message['erreur_nom'])) { ?>
+              <small class="text-danger"><?php echo $message['erreur_nom']?></small>
+            <?php 
+            } 
+            ?>
+          </div>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-6">
+          <div class="form-group">
+            <input type="text" name="prenom" id="prenom" class="form-control input-lg" placeholder="Prénom" tabindex="2" required>
+             <!-- Afficher un message d'erreur s'il y'a lieu -->
+            <?php  if(isset($message['erreur_prenom'])) { ?>
+              <small class="text-danger"><?php echo $message['erreur_prenom']?></small>
+            <?php 
+            } 
+            ?>
+          </div>
+        </div>
+      </div>
+      <div class="form-group">
+        <input type="text" name="username" id="username" class="form-control input-lg" placeholder="Nom d'utilisateur" tabindex="3" required>
+         <!-- Afficher un message d'erreur s'il y'a lieu -->
+            <?php  if(isset($message['erreur_nomUsager'])) { ?>
+               <small class="text-danger"><?php echo $message['erreur_nomUsager']?></small>
+            <?php 
+            } 
+            ?>
+      </div>
+      <div class="row">
+        <div class="col-xs-12 col-sm-6 col-md-6">
+          <div class="form-group">
+            <input type="password" name="password" id="password" class="form-control input-lg" placeholder="Mot de passe" tabindex="5" required>
+            <!-- Afficher un message d'erreur s'il y'a lieu -->
+            <?php  if(isset($message['erreur_motDePasse'])) { ?>
+              <small class="text-danger"><?php echo $message['erreur_motDePasse']?></small>
+            <?php 
+            } 
+            ?>
+            
+          </div>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-6">
+          <div class="form-group">
+            <input type="password" name="passwordRepeat" id="passwordRepeat" class="form-control input-lg" placeholder="Confirmer le mot de passe" tabindex="6" required>
+             <!-- Afficher un message d'erreur s'il y'a lieu -->
+            <?php  if(isset($message['erreur_motDePasseConfirm'])) { ?>
+              <small class="text-danger"><?php echo $message['erreur_motDePasseConfirm']?></small>
+            <?php 
+            } 
+            ?>
+          </div>
 
-			  <label for="inputPassword" class="sr-only">Mot de passe</label>
-			  <input type="password" id="inputPassword" name="password" class="form-control" placeholder="Mot de passe" required>
-
-        <label for="inputPassword" class="sr-only">Entrer encore mot de passe</label>
-        <input type="password" id="inputPassword" name="passwordRepeat" class="form-control" placeholder="Entrer encore mot de passe" required>
-
-			  <label for="inputEmail" class="sr-only">Prenom usager</label>
-			  <input type="text" id="inputEmail" name="prenom" class="form-control" placeholder="Prenom usager" required autofocus>
-
-			  <label for="inputEmail" class="sr-only">Nom usager</label>
-			  <input type="text" id="inputEmail" name="nom" class="form-control" placeholder="Nom usager" required autofocus>
-
-
-			 <!--  <div class="checkbox mb-3">
-			    <label>
-			      <input type="checkbox" value="remember-me">Se souvenir de moi
-			    </label>
-			  </div> -->
-			
-			<input type="submit"  value="Enregistrer"/>
-			<input type="hidden" name="requete" value="Enregistrer">
-      </form>
-			<?php
-				}
-				else
-				{
-					echo "<p>Vous êtes déjà connecté sous le nom " . $_SESSION["UserName"] . "</p>";
-					echo "<a href='index.php?requete=Logout'>Se déconnecter</a>";
-				}
-			if(isset($messageErreur))
-				echo "<p>$messageErreur</p>";
-		    ?>	
-		    <p class="mt-5 mb-3 text-muted">&copy; 2019-2020</p>
+        </div>
+        
+          
+      </div>
+            
+      <hr class="colorgraph">
+      <div class="row">
+        <div class="col-xs-12 col-md-6"><input type="submit" value="S'enregistrer" class="btn btn-primary btn-block btn-lg" tabindex="7"></div>
+        <input type="hidden" name="requete" value="Enregistrer">
+        <div class="col-xs-12 col-md-6"><a href="index.php?requete=Login" class="btn btn-success btn-block btn-lg">Se connecter</a></div>
+      </div>
+    </form>
+      <?php
+        }
+        else
+        {
+          echo "<p>Vous êtes déjà connecté sous le nom " . $_SESSION["UserName"] . "</p>";
+          echo "<a href='index.php?requete=Logout'>Se déconnecter</a>";
+        }
+        ?>  
+        <p class="mt-5 mb-3 text-muted">&copy; 2019-2020</p>
         </div>
     </div>  
   </body>
