1ff7f701f3c6eed5aa706164a7a8fe34dbc9f958
foudil-benzaid <48103976+foudil-benzaid@users.noreply.github.com>
2019-03-30 14:42:53 -0400

    MAJ du modèle Usager.class

--- modeles/Usager.class.php
+++ modeles/Usager.class.php
@@ -64,6 +64,55 @@ class Usager extends Modele {
 	}
 
 	/**
+	* Fonction de modification des informations d'un usager
+	* 
+	* @param $id l'id de l'usager
+	* @param $username le nom d'utilisateur 
+	* @param $password le mot de passe de l'usager
+	* @param $nom le nom de l'usager
+	* @param $prenom le prénom de l'usager
+	* @return $resultat résultat de la requête
+	*/
+	public function modifierUsager($id, $username, $password, $nom, $prenom)
+	{
+		// CAS 1 : En plus des informations de l'usager on MAJ le mot de passe
+		if (trim($password) != '') {
+						
+			$password = md5($password);
+			$sql = "UPDATE vino_usagers SET 
+	    		username=?, 
+	    		password=?, 
+	    		nom=?, 
+	    		prenom=?
+	    		WHERE vino_usagers.id = $id";
+
+			$stmt = $this->_db->prepare($sql);
+			$stmt->bind_param('ssss', $username, $password, $nom, $prenom);
+
+		// CAS 2 : Uniquement les informations de l'usagers
+		}else{
+
+			$sql = "UPDATE vino_usagers SET 
+	    		username=?, 
+	    		nom=?, 
+	    		prenom=?
+	    		WHERE vino_usagers.id = $id";
+
+			$stmt = $this->_db->prepare($sql);
+			$stmt->bind_param('sss', $username, $nom, $prenom);
+
+		}
+				
+        $resultat = $stmt->execute();
+        var_dump($resultat);
+        if($resultat) {
+        	$resultat = $this->_db->query("SELECT * FROM vino_usagers WHERE vino_usagers.id = $id");
+        	$resultat = $resultat->fetch_assoc();
+        }
+        return $resultat;
+	}
+
+	/**
 	* Fonction pour récupérer la liste de celliers par usager
 	* 
 	* @param $idUsager l'id de l'usager
@@ -83,7 +132,6 @@ class Usager extends Modele {
 					vino_bouteille.url_saq,
 					vino_bouteille.url_img,
 					vino_bouteille.format,
-					vino_bouteille.vote,
 					vino_bouteille.garde_jusqua,
 					vino_bouteille.millesime,
 					vino_bouteille.quantite,
@@ -96,7 +144,7 @@ class Usager extends Modele {
         			JOIN vino_usagers ON vino_usagers.id=vino_cellier.usager_id 
 
         			WHERE vino_cellier.usager_id = ".$idUsager;
- 		//die($requete);
+ 		
 		if(($res = $this->_db->query($requete)) ==	 true)
 		{
 			if($res->num_rows)
@@ -113,7 +161,7 @@ class Usager extends Modele {
 			throw new Exception("Erreur de requête sur la base de donnée", 1);
 			 
 		}
-       
+
 		return $rangees;
 	}
 	/**
@@ -124,36 +172,21 @@ class Usager extends Modele {
 	*/
 	public function testUser($usager)
 	{
-			
-	//		$requete = "SELECT * from vino_cellier JOIN vino_bouteille ON vino_cellier.id_bouteille=vino_bouteille.id WHERE vino_cellier.id_usager = ".$_SESSION["UserID"];
-	       			
-			$requete = "SELECT COUNT(id) as c FROM vino_usagers WHERE username= '".$usager."'";
-			$resultat = $this->_db->query($requete);
-	        $rangee = $resultat->fetch_assoc();
-			return $rangee;
+			       			
+		$requete = "SELECT COUNT(id) as c FROM vino_usagers WHERE username= '".$usager."'";
+		$resultat = $this->_db->query($requete);
+        $rangee = $resultat->fetch_assoc();
+		return $rangee;
 
 	}
 	/**
-	* Fonction pour récupérer des usagers 
+	* Fonction pour changer le mot de passe
 	* 
 	* @param $usager nom de l'utilisateur
+	* @param $password le mot de passe
+	* @param $passwordNouveau le nouveau mot de passe
 	* @return $rangees résultat de la requête
 	*/
-	public function prendreUsagers()
-	{
-				       			
-			$requete = "SELECT vino_usagers.*, GROUP_CONCAT(vino_cellier.nom  SEPARATOR ', ') as cnom FROM vino_usagers 
-						left JOIN vino_cellier ON vino_usagers.id=vino_cellier.usager_id
-						WHERE vino_usagers.admin='non' GROUP BY vino_usagers.id";
-			$resultat = $this->_db->query($requete);
-			$rangees=array();
-			while($rangee = $resultat->fetch_assoc())
-			{
-	        	$rangees[] = $rangee;
-	   		}
-			return $rangees;
-
-	}
 	public function ChangerMotDePass($usager, $password, $passwordNouveau)
 	{
 		$requete = "SELECT * from vino_usagers WHERE username = '" . $usager . "' AND password = '".md5($password)."'";
@@ -173,23 +206,79 @@ class Usager extends Modele {
 			return false;
 		}
 	}
-	public function supprimerUsager($id)
-	{
-			$requete = "SELECT id FROM vino_cellier WHERE vino_cellier.usager_id = $id";
-			$res = $this->_db->query($requete);
-			while($r = $res->fetch_assoc()) {
-				$this->_db->query("DELETE from vino_bouteille WHERE cellier_id = ".$r['id']);
-			}
 
-			$requete = "DELETE from vino_cellier WHERE vino_cellier.usager_id = $id";
-			if($this->_db->query($requete)) {
-				$requete = "DELETE from vino_usagers WHERE vino_usagers.id = $id";
-				return $this->_db->query($requete);
-			}
-			return false;
+	/**
+	* Fonction pour récupérer nombre d'usagers
+	* @return $rangees résultat de la requête
+	*/
+	public function nombreUsagers()
+	{
+			       			
+		$requete = "SELECT COUNT(id) as nombreUsagers FROM vino_usagers";
+		$resultat = $this->_db->query($requete);
+        $rangee = $resultat->fetch_assoc();
+		return $rangee;
 
 	}
 
+	/**
+	* Fonction pour récupérer les informations d'un usager
+	* @return $rangees résultat de la requête
+	*/
+	public function obtenirUsager($id)
+	{
+			       			
+		$requete = "SELECT  * FROM vino_usagers WHERE id=$id";
+		$resultat = $this->_db->query($requete);
+        $rangee = $resultat->fetch_assoc();
+		return $rangee;
+
+	}
+
+
+	/**
+	* Fonction pour récupérer des usagers 
+	* 
+	* @return $rangees résultat de la requête
+	*/
+	public function listeUsagers()
+	{
+				       			
+		$requete = "SELECT vino_usagers.*, GROUP_CONCAT(vino_cellier.nom  SEPARATOR ', ') as cnom FROM vino_usagers 
+					left JOIN vino_cellier ON vino_usagers.id=vino_cellier.usager_id
+					WHERE vino_usagers.admin='non' GROUP BY vino_usagers.id";
+		$resultat = $this->_db->query($requete);
+		$rangees=array();
+		while($rangee = $resultat->fetch_assoc())
+		{
+        	$rangees[] = $rangee;
+   		}
+		return $rangees;
+
+	}
+
+	/**
+	* Fonction de suppression d'un usager
+	* 
+	* @param $id l'id de l'usager
+	* @return $rangees résultat de la requête
+	*/
+	public function supprimerUsagers($id)
+	{
+		$requete = "SELECT id FROM vino_cellier WHERE vino_cellier.usager_id = $id";
+		$res = $this->_db->query($requete);
+		while($r = $res->fetch_assoc()) {
+			$this->_db->query("DELETE from vino_bouteille WHERE cellier_id = ".$r['id']);
+		}
+
+		$requete = "DELETE from vino_cellier WHERE vino_cellier.usager_id = $id";
+		if($this->_db->query($requete)) {
+			$requete = "DELETE from vino_usagers WHERE vino_usagers.id = $id";
+			return $this->_db->query($requete);
+		}
+		return false;
+
+	}
 
 }
 
