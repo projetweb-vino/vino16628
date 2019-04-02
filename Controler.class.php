4ba457f23ee54d66db9fc8b712a7fc6691f64cb7
foudil-benzaid <48103976+foudil-benzaid@users.noreply.github.com>
2019-04-02 15:19:30 -0400

    Récupérer la liste des bouteilles depuis la SAQ

--- Controler.class.php
+++ Controler.class.php
@@ -439,8 +439,8 @@ class Controler
 				// Récupérer la liste des celliers par usager
 				$data = $bte->CellierParUsager($_SESSION["UserID"] );
 				// Récupérer la liste des bouteilles
-				//$dat = $bte->ListeBouteilleSAQ();
-				$dat = $bte->getListeBouteille();
+				$dat = $bte->ListeBouteilleSAQ();
+				// $dat = $bte->getListeBouteille();
 				// Récupérer les types
 				$datas = $bte->RecupererTypes();
 				$nombreSAQ = $this->nombreSAQ();
