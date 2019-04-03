095d1174ab2501f9b266dbff36142e5711d744a3
foudil-benzaid <48103976+foudil-benzaid@users.noreply.github.com>
2019-04-02 23:06:11 -0400

    Corriger le problème d'encodage d'importation des bouteilles SAQ

--- lib/SAQ.class.php
+++ lib/SAQ.class.php
@@ -86,24 +86,25 @@ class SAQ extends Modele {
 		$p = $noeud -> getElementsByTagName("p");
 		foreach ($p as $node) {
 			if ($node -> getAttribute('class') == 'nom') {
-				$info -> nom = utf8_decode(trim($node -> textContent));
+				$info -> nom = trim($node -> textContent);
 			} else if ($node -> getAttribute('class') == 'desc') {
 				$info -> desc = new stdClass();
 				$info -> desc -> texte = $node -> textContent;
 				$res = preg_match_all("/\r\n\s*(.*)\r\n/", $info -> desc -> texte, $aDesc);
 				if (isset($aDesc[1][2])) {
 					preg_match("/\d{8}/", $aDesc[1][2], $aRes);
-					$info -> desc -> code_SAQ = utf8_decode(trim($aRes[0]));
+					$info -> desc -> code_SAQ = trim($aRes[0]);
 				}
 				if (isset($aDesc[1][1])) {
 					preg_match("/(.*),(.*)/", $aDesc[1][1], $aRes);
-					$info -> desc -> pays = utf8_decode(trim($aRes[1]));
-					$info -> desc -> format = utf8_decode(trim($aRes[2]));
+					$info -> desc -> pays = trim($aRes[1]);
+				    // 	Enlever l'espace 
+					$info -> desc -> format = substr(trim($aRes[2]), 2);
 				}
 				if (isset($aDesc[1][0])) {
-					$info -> desc -> type = utf8_decode(trim($aDesc[1][0]));
+					$info -> desc -> type = trim($aDesc[1][0]);
 				}
-				$info -> desc -> texte = utf8_decode(trim($info -> desc -> texte));
+				$info -> desc -> texte = trim($info -> desc -> texte);
 			}
 		}
 		$p = $noeud -> getElementsByTagName("td");
@@ -111,7 +112,7 @@ class SAQ extends Modele {
 			if ($node -> getAttribute('class') == 'price') {
 				$info -> prix = trim($node -> textContent);
 				preg_match("/ \r\n(.*)$/", $info -> prix, $aRes);
-				$info -> prix = utf8_decode(trim($aRes[1]));
+				$info -> prix = trim($aRes[1]);
 				// Remplacer la virgule avec un point
 				$info -> prix = preg_replace("/[,]/", '.', $info -> prix);
 			}
