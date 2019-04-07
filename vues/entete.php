b6e57f4c13ba2aeae95b4fc96e5499079d721419
OKBA BENAISSA <okbabenaisa@hotmail.com>
2019-04-07 01:45:53 -0400

    MAj les lient

--- vues/entete.php
+++ vues/entete.php
@@ -6,12 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Vino</title>
-    <!--=====================================FontAwesome==========================================-->
-    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
-    <!--===============================================================================================-->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <!--===============================================================================================-->
+    
+    <!--================================FontAwesome===============================================-->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
     <!--===============================================================================================-->
     <link rel = "stylesheet" href = "https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css" >
     <!-- Police Google -->
@@ -21,7 +20,7 @@
     <!-- Style Base -->
     <link rel="stylesheet" href="./css/base_h5bp.css" type="text/css" media="screen">
     <!-- FontAwesome -->
-    <link rel="stylesheet" href="./css/fontawesome.min.css">
+    <!-- <link rel="stylesheet" href="./css/fontawesome.min.css"> -->
     <!-- CDN Bootstrap -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <!-- Notre style css -->
